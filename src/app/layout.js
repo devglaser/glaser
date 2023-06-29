434be657ed0d00ab13de83880fcd1b204dc1fca0
Daniel_GLaser <128993292+devglaser@users.noreply.github.com>
2023-06-29 11:48:52 -0300

    Apenas pra buildar

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,13 +8,12 @@ export default function RootLayout({ children }) {
     <html lang="pt-br">
       <head>
         <title>Desenvolvedor & Designer Web - Daniel_GLaser</title>
-        <link rel='icon' href='./logo.svg'/>
         
-        <meta name="description" content="Especialista em desenvolvimento & design de sites minimalistas"/>
-        <meta name="keywords" content="website, site, web, landing page, e-commerce, one page"/>
+        <link rel='icon' href='./logo.svg'/>
         <link rel="canonical" href="https://glaser.vercel.app"/>
 
-        
+        <meta name="keywords" content="website, site, web, landing page, e-commerce, one page"/>
+        <meta name="description" content="Especialista em desenvolvimento & design de sites minimalistas"/>
         <meta property="og:title" content="Desenvolvedor & Designer Web - Daniel_GLaser"/>
         <meta property="og:description" content="Especialista em desenvolvimento & design de sites minimalistas"/>
         <meta property="og:image" content="https://glaser.vercel.app/imgs/imagem.jpg"/>
